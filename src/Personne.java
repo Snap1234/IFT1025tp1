f5ed9440d740fc6cb4eb20632e032ce3ef03f5c9
soukainabenabid <soukainabenabid@mbp-de-soukaina>
2020-11-06 21:59:17 -0500

    update

--- src/Personne.java
+++ src/Personne.java
@@ -3,10 +3,13 @@ public abstract class Personne {
 	String prenom;
 	String nom;
 	int dateDeNaissance;
+	String mail;
 	
-	public Personne(String prenom, String nom, int dateDeNaissance) {
+	public Personne(String prenom, String nom, int dateDeNaissance, String mail) {
 		this.prenom = prenom;
 		this.nom = nom;
 		this.dateDeNaissance = dateDeNaissance;
+		this.mail = mail;
 	}
+	
 }
